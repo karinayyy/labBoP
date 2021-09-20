578c76b2df1469317b614ec9a0b43ebfe62a267a
karpenko-karina <karinakarpenko.k@gmail.com>
2021-09-20 11:26:55 +0300

    y not overwritting

--- lab2/task2.cpp
+++ lab2/task2.cpp
@@ -6,24 +6,25 @@ using namespace std;
 int main()
 {
     int n, k, x, i;
-    float y;
+    double y;
     cout << "Enter n, k, x" << endl;
     cin >> n >> k >> x;
     if (n >= 0){
         if (k <= n){
-            y = 0;
-            i = 1;
-            while(i <= n){
+            y = 0; 
+            i = 1;    
+            while (i <= n){
                 if(i != k){
                     if ((x+2*i) != 0){
-                        y += i/(x+2*i);
+                        y += ((i) /( x+(2*i)));
                         i++;
+                        cout << "YY" << y << endl;
                     }else{
                         cout << "x+2i == 0 Error"<< endl;
-                    }
+                    } 
                 }else{
                     i++;
-                }
+                } 
                 cout << "y = " <<  y << ", i = " << i << endl;
             }
             cout << "Y = " << y << endl;
