5eb644cba517f5527f56e0681b0066518cabc878
karpenko-karina <karinakarpenko.k@gmail.com>
2021-09-22 10:26:31 +0300

    taskThreeDone

---
+++ lab2/task3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x, n, y, i;
+    cout << "Enter x, n " << endl;
+    cin >> x >> n;
+    if (n >= 0){
+        y = 1;
+        i = 1;
+        while(i <= n){
+            y *= (x - 2*n);
+            i++;
+            cout << "y = " << y << " i = " << i << endl;
+        }
+        cout << "Y = " << y << endl;
+    }else{
+        cout << "Wrong n" << endl;
+    }
+}
