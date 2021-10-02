fee734d8b9b1b2ff2306ffb45b81e3dda83975a5
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-02 19:43:21 +0300

    taskTwoAddedBracketsDone

--- lab2/task1.cpp
+++ lab2/task1.cpp
@@ -12,11 +12,11 @@ int main()
         if(b == 0)
             c == 0 ? cout << "Бессконечное кол-во корней" << endl : cout << "Нет коренй" << endl;
         else{
-            double x1 = -c/b;
+            double x1 = -c / b;
             cout << "x1 = " << x1;
         }
     }else{
-        double d = pow(b, 2) - 4 * a * c;
+        double d = pow(b, 2) - (4 * a * c);
         cout << "d = " << d << endl;
         if (d < 0)
             cout << "Нет корней" << endl;
@@ -24,11 +24,11 @@ int main()
             double x1, x2;
             d = sqrt(d);
             cout << "sqrt(d) = " << d << endl;
-            x1 = (-b + d) / (2*a);
-            x2 = (-b - d) / (2*a);
+            x1 = (-b + d) / (2 * a);
+            x2 = (-b - d) / (2 * a);
             cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
         }else{
-            double x1 = -b / 2*a;
+            double x1 = -b / (2 * a);
             cout << "x1 = " << x1 << endl;
         }
     }
