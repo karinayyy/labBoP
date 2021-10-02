7b5745bd5d1cb4af8fc1bc96fd5efd366b3454c7
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-02 19:36:54 +0300

    taskOneAddedSomeChanges

--- lab2/task1.cpp
+++ lab2/task1.cpp
@@ -1,46 +1,36 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-    double a, b, c, x1, x2, d;
+    double a, b, c;
     cout << "Введите a, b, c " << endl;
     cin >> a >> b >> c;
-    if (a == 0) {
-        if (b == 0) {
-            cout << "Бесконечное количевство корней" << endl;
+    if(a == 0){
+        if(b == 0)
+            c == 0 ? cout << "Бессконечное кол-во корней" << endl : cout << "Нет коренй" << endl;
+        else{
+            double x1 = -c/b;
+            cout << "x1 = " << x1;
         }
-
-        else if (b != 0) {
-            if (c == 0) {
-                cout << "Нет корней" << endl;
-            }
-            else if (c != 0) {
-                x1 = -(c / b);
-                cout << "x1 = " << x1 << endl;
-            }
-        }
-    }
-
-    else if (a != 0) {
-        d = pow(b, 2) - 4 * a * c;
-        cout << "D = " << d << endl;
-        if (d < 0) {
+    }else{
+        double d = pow(b, 2) - 4 * a * c;
+        cout << "d = " << d << endl;
+        if (d < 0)
             cout << "Нет корней" << endl;
-        }
-        else if (d > 0) {
-            x1 = (-(b) + sqrt(d)) / (2 * a);
-            x2 = (-(b) - sqrt(d)) / (2 * a);
-            cout << "x1 = " <<  x1 << ", x2 =  " << x2 << endl;
-        }
-        else if (d == 0) {
-            x1 = -(b / (2 * a));
-            cout << "x1 = " <<  x1 << endl;
+        else if (d > 0){
+            double x1, x2;
+            d = sqrt(d);
+            cout << "sqrt(d) = " << d << endl;
+            x1 = (-b + d) / (2*a);
+            x2 = (-b - d) / (2*a);
+            cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
+        }else{
+            double x1 = -b / 2*a;
+            cout << "x1 = " << x1 << endl;
         }
     }
-
     return 0;
 }
