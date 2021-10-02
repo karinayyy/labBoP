b4dfe080dd6584e19bb0611026402727b81176df
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-02 20:58:03 +0300

    taskFiveDone

--- lab2/task5.cpp
+++ lab2/task5.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a, b, h, n, x, i, y, j, s;
-    cout << "Введите входные данные (интервал a-b, шаг h, n) " << endl;
+    int a, b, n;
+	double h;
+    cout << "Введите входные данные (интервал a-b(a <= b), шаг h > 0, n > 1)" << endl;
     cin >> a >> b >> h >> n;
-    if ((a <= b)&&(h > 0)&&(n > 1)){
-		    x = a;
-		    do {
+    if (a <= b && h > 0 && n > 1){
+		    double x = a;
+		    while (x <= b){
 		        if (x < 0) {
-		            y = 1;
-		            i = 1;
-		            do {
-		                s = 0;
-		                j = 1;
-		                do {
+		            double y = 1;
+		            int i = 1;
+		            while (i <= (n - 1)){
+		                double s = 0, j = 1;
+		                while (j <= n){
 		                    s += pow((x - pow(i, 2) + j), 2);
 		                    j++;
-		                } while (j <= n);
+		                }
 		                y *= s;
 		                i++;
-		            } while (i <= (n - 1));
+		            }
+		        cout << "x = " << x << " y = " << y << endl;
 		        }
-
 		        else if (x >= 0) {
-		            y = 0;
-		            i = 0;
-		            do {
+		            double y = 0, i = 0;
+		            while (i <= (n - 1)){
 		                y += (x - 1) / (i + 1);
 		                i++;
-		            } while (i <= (n - 1));
-		        }
+		            }
 		        cout << "x = " << x << " y = " << y << endl;
+		        }
 		        x += h;
-		    } while (x <= b);
+		    }
 		    cout << "end" << endl;
-	} else {
+	} else
 		cout << "Введите: (a <= b)&&(h > 0)&&(n > 1) " << endl;
-	}
+	return 0;
 }
