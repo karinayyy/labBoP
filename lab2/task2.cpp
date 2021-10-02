e21d696950bb3badabeea047136eccd8650b3d55
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-02 19:54:07 +0300

    taskTwoAddedSomeChanges

--- lab2/task2.cpp
+++ lab2/task2.cpp
@@ -9,8 +9,7 @@ int main()
     double x, y;
     cout << "Enter n, k, x" << endl;
     cin >> n >> k >> x;
-    if (n >= 0){
-        if (k <= n){
+    if (n >= 0 && k <= n){
             y = 0; 
             i = 1;    
             while (i <= n){
@@ -18,21 +17,14 @@ int main()
                     if ((x+2*i) != 0){
                         y += i/( x+2*i);
                         i++; 
-                    }else{
+                    }else
                         cout << "x+2i == 0 Error"<< endl;
-                    } 
-                }else{
+                }else
                     i++;
-                } 
                 cout << "y = " <<  y << ", i = " << i << endl;
             }
             cout << "Y = " << y << endl;
-        }else{
-            cout << "Wrong k" << endl;
-        }
-    }else{
-        cout << "Wrong n" << endl;
-    }
-
+    }else
+        cout << "Enter n>= 0 && k <= n" << endl;
     return 0;
 }
