3a8c9f42c9ba381f7c4af964794672aa710ade96
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-02 18:33:30 +0300

    taskTwoAddedSomeChanges

--- lab1/task2.cpp
+++ lab1/task2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    int a, b, c, d, e, f, g, h, result;
+    double a, b, c, d, e, f, g, h, result;
     cout << "Введите 8 цифр чтобы получить среднее арифметическое " << endl;
     cin >> a >> b >> c >> d >> e >> f >> g >> h;
     result = (a + b + c + d + e + f + g + h) / 8;
