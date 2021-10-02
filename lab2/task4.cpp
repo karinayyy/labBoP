3613244a288807a7f58a8a787f6e746dc587ade6
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-02 20:22:35 +0300

    taskFourAddedSomeChanges

--- lab2/task4.cpp
+++ lab2/task4.cpp
@@ -5,17 +5,15 @@ using namespace std;
 
 int main()
 {
-    double y, add, eps;
-    int i;
+    double y, i, add, eps;
     cout << "Enter eps" << endl;
     cin >> eps;
     y = 0;
-    add = 1;
     i = 1;
-    while(add > eps){
+    do{
         y += 1/(pow(2,i));
         add = 1/(pow(2,i));
         i++;
         cout << "i = " << i << endl << "y = " << y << endl << "add = " << add << endl;
-    }
+    }while(add > eps);
 }
