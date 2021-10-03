bb26778c0db56f4e3755c6408a7d4e17266a3413
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-03 15:32:13 +0300

    taskSixRedone

--- lab2/task6.cpp
+++ lab2/task6.cpp
@@ -4,10 +4,20 @@ using namespace std;
 
 int main()
 {
-    char symbol;
-    cout << "Enter the symbol" << endl;
-    cin >> symbol;
-    int num = symbol;
-    cout << "Hex code of entered char = " << hex << num << endl;
+    cout << "Enter n or s" << endl;
+    char chose;
+    cin >> chose;
+    if (chose == 'n'){
+        int num;
+        cout << "Enter num" << endl;
+        cin >> num; 
+        cout << "Hex code of entered char = " << hex << num << endl;
+    }else if (chose == 's'){
+        char symbol;
+        cout << "Enter the symbol" << endl;
+        cin >> symbol;
+        int num = symbol;
+        cout << "Hex code of entered char = " << hex << num << endl;
+    }
     return 0;
 }
