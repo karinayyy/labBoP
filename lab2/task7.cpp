d53a77b0dc34b8748acaf61f56a3f85269a535c1
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-03 12:28:29 +0300

    taskSevenDone

---
+++ lab2/task7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+    char symbol;
+    cout << "Enter the symbol" << endl;
+    cin >> symbol;
+    int codeOfTheSumbol = symbol;
+    cout << "Code of the entered symbol = " << codeOfTheSumbol << endl;
+    return 0;
+}
