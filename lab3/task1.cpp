04f1c4e0b62634d7911f655a2f3b748676c77402
karpenko-karina <karinakarpenko.k@gmail.com>
2021-10-25 21:49:08 +0300

    taskOneDontWork

--- lab3/task1.cpp
+++ lab3/task1.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-double localVar()
+int minMax(int i)
 {
-    static double a, b;
-    char eqs = 'e';
-    cout << "Enter a, b" << endl;
-    cin >> a >> b;
-    return (a != b ? (a < b ? (a) : (b) ) : (eqs));
+    static int min = 0;
+    static int max = 0;
+    if (i < max){
+        min = i;
+    }if(i > max){
+        max = i;
+    }
+    return min, max;
 }
 
 int main()
 {
-    int num;
-    cout << "Enter '0' to quit or '1' to continue" << endl;
-    cin >> num;
-    if (num == 1){
-        cout << localVar() << endl;
-        main();
-    }else if (num == 0)
-        return 0;
-    else
-        main();
+    int i;
+    do{
+        cout << "Enter i" << endl;
+        cin >> i;
+        cout << minMax(i) << endl;
+    }while (i);
+    return 0;
 }
