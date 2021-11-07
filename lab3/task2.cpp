b64b74495f95dc585065a466a82fc6fcffc149c5
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-07 20:31:24 +0200

    taskOneDone

--- lab3/task2.cpp
+++ lab3/task2.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 double sum(int n, double x)
 {
-    return n <= 1 ? 1 : x + sum(n - 1, x);
+    if(n < 1){
+        return 1;
+    } else{
+        return (x + n) * sum(n - 1, x); // n = 4 x = 5 (4+5)(3+5)(2+5)(1+5)=3024
+    }
 }
 
 int main()
 {
     int n;
     double x;
+    cout << "Enter n, x" << endl;
     cin >> n >> x;
     cout << sum(n, x);
     return 0;
