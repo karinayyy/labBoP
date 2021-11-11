c5161470019484c3a4961145949ca2eb45dd1c19
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-12 01:13:43 +0200

    taskFiveDontWork

--- lab3/task5.cpp
+++ lab3/task5.cpp
@@ -7,11 +7,12 @@ bool readData(double &, double &, double &, double &);
 double yFunc(double, double);
 
 int main(){
-    double a, b, h, y, n, x;
-    if(readData(a, b, h, n)){
+    double a, b, h, n;
+    if(readData(a, b, h, n)){    
         double x = a;
         while(x <= b){
-            cout << "x = " << x << " y = " << y << yFunc(x, n) << endl;
+            cout << yFunc(x, n) << endl;
+            cout << "x = " << x << endl;
             x+=h;
         }
         return 0;
@@ -30,28 +31,24 @@ bool readData(double& a, double& b, double& h, double& n){
     }
 }
 
+
 double yFunc(double x, double n){
     double y;
-        if (x < 0) {
-            y = 1;
-            int i = 1;
-            while (i <= (n - 1)){
-                double s = 0, j = 1;
-                while (j <= n){
-                    s += pow((x - pow(i, 2) + j), 2);
-                    j++;
-                }
-                y *= s;
-                i++;
+    if (x < 0) {
+        y = 1;
+        for (int i = 1; i <= (n - 1); i++){
+            double s = 0;
+            for (int j = 1; j <= n; j++){
+                s += pow((x - pow(i, 2) + j), 2);
             }
+            y *= s;
         }
-        else if (x >= 0) {
-            double i = 0;
-            y = 0;
-            while (i <= (n - 1)){
-                y += (x - 1) / (i + 1);
-                i++;
-            }
+    }
+    else if (x >= 0) {
+        y = 0;
+        for (int i = 0; i <= (n - 1); i++){
+            y += (x - 1) / (i + 1);
         }
+    }
     return y;
 }
