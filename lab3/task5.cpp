128918de840ac8ab6df2dac655b13bc77dc1a368
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-11 13:58:35 +0200

    add0

--- lab3/task5.cpp
+++ lab3/task5.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 
 bool readData(double &, double &, double &, double &);
-double yFunc(double, double, double, double, double, double);
+double yFunc(double, double);
 
 int main(){
     double a, b, h, y, n, x;
     if(readData(a, b, h, n)){
-            cout << yFunc(x, n, h, y, a, b) << endl;
+        double x = a;
+        while(x <= b){
+            cout << "x = " << x << " y = " << y << yFunc(x, n) << endl;
+            x+=h;
+        }
         return 0;
     }
-    return -1;
 }
 
 bool readData(double& a, double& b, double& h, double& n){
@@ -27,31 +30,28 @@ bool readData(double& a, double& b, double& h, double& n){
     }
 }
 
-double yFunc(double x, double n, double h, double y, double a, double b){
-        x = a;
-        while (x <= b){
-            if (x < 0) {
-                int i = 1;
-                y = 1;
-                while (i <= (n - 1)){
-                    double s = 0, j = 1;
-                    while (j <= n){
-                        s += pow((x - pow(i, 2) + j), 2);
-                        j++;
-                    }
-                    y *= s;
-                    i++;
+double yFunc(double x, double n){
+    double y;
+        if (x < 0) {
+            y = 1;
+            int i = 1;
+            while (i <= (n - 1)){
+                double s = 0, j = 1;
+                while (j <= n){
+                    s += pow((x - pow(i, 2) + j), 2);
+                    j++;
                 }
+                y *= s;
+                i++;
             }
-            else if (x >= 0) {
-                int i = 0;
-                y = 0;
-                while (i <= (n - 1)){
-                    y += (x - 1) / (i + 1);
-                    i++;
-                }
-            }
-            x+=h;
         }
-        return y;
+        else if (x >= 0) {
+            double i = 0;
+            y = 0;
+            while (i <= (n - 1)){
+                y += (x - 1) / (i + 1);
+                i++;
+            }
+        }
+    return y;
 }
