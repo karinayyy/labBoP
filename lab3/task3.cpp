9d759700f5ff7e8e8ef649598af8b978f558e0ce
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-13 13:39:53 +0200

    taskThreeDone

--- lab3/task3.cpp
+++ lab3/task3.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
-double firstSum(){
-    return 1;
-}
-
 double sum(double x){
     return x;
 }
 
-double sum(double x, double y, double z){
-    return x * y * z;
+double sum(double x, double y){
+    return x * y;
+}
+
+double sum(){
+    return 1;
+}
+
+double sum1(double x = 1, double y = 1){
+    return x * y;
 }
 
 int main(){
-    cout << firstSum() << endl;
+    cout << sum() << endl;
     cout << sum(3) << endl;
-    cout << sum(1, 2, 3) << endl;
+    cout << sum(1, 2) << endl;
+    cout << sum1() << endl;
+    cout << sum1(3) << endl;
+    cout << sum1(1, 2) << endl;
+    
     return 0;
 }
