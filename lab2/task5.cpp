6610c5c173a4601cc78f373486e1fc9d5ea34aac
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-13 13:36:57 +0200

    taskFiveDone

--- lab2/task5.cpp
+++ lab2/task5.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main()
 {
     int n;
-	double h, a, b;
+	double h, a, b, y;
     cout << "Введите входные данные (интервал a-b(a <= b), шаг h > 0, n > 1)" << endl;
     cin >> a >> b >> h >> n;
     if (a <= b && h > 0 && n > 1){
 		    double x = a;
 		    while (x <= b){
 		        if (x < 0) {
-		            double y = 1;
+		            y = 1;
 		            int i = 1;
 		            while (i <= (n - 1)){
 		                double s = 0, j = 1;
@@ -24,19 +24,18 @@ int main()
 		                y *= s;
 		                i++;
 		            }
-		        cout << "x = " << x << " y = " << y << endl;
 		        }
 		        else if (x >= 0) {
-		            double y = 0, i = 0;
+		            double i = 0;
+					y = 0;
 		            while (i <= (n - 1)){
 		                y += (x - 1) / (i + 1);
 		                i++;
 		            }
-		        cout << "x = " << x << " y = " << y << endl;
 		        }
 		        x += h;
+		    cout << "x = " << x << " y = " << y << endl;
 		    }
-		    cout << "end" << endl;
 	} else
 		cout << "Введите: (a <= b)&&(h > 0)&&(n > 1) " << endl;
 	return 0;
