1e56d7b30680890aa2ea0d9ee4050595d3121de9
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-14 17:15:14 +0200

    taskSixAddedSomeChanges

--- lab3/task6.cpp
+++ lab3/task6.cpp
@@ -10,12 +10,13 @@ using namespace std;
 //     return y;
 // }
 
-double signum(int n, double y = 0){
-    return n <= 1 ? 1 : y + n + signum(n - 1, y);
+double signum(int n){
+    return n <= 1 ? 1 : n + signum(n - 1);
 }
 
 int main(){
     cout << signum(5) << endl; //5+4+3+2+1=15
+    cout << signum(1) << endl;
     cout << signum(10) << endl;
     cout << signum(15) << endl;
     return 0;
