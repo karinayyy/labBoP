815b68248ce10f3906474d17efc87de8a3d56c5d
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-14 17:20:16 +0200

    taskNineDone

---
+++ lab3/task9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double sum(int x){
+    return x < 1 ? 1 : pow(M_E,x) + sum(x - 1) - 1;
+}
+
+int main(){
+    cout << pow(M_E,1) << endl;
+    cout << sum(1) << endl;
+    cout << sum(2) << endl;
+    cout << sum(5) << endl;
+    return 0;
+}
