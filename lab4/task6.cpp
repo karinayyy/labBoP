288f2f142d83b42e84cb7942ea21666def391b83
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-22 20:09:42 +0200

    taskSixDone

---
+++ lab4/task6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+void bin(int number)
+{
+    if(number)
+    {
+        cout << number % 2;
+        bin(number / 2);
+
+    }
+}
+int main(){
+    int number;
+    cin >> number;
+    bin(number);
+    return 0;
+}
