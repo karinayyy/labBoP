5c3b49660cdc685d0500c51fd39abb05d280a10f
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-22 20:37:29 +0200

    taskNineDone

---
+++ lab4/task9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int size = 5;
+    int arr[size];
+    int sum = 0;
+    cout << "Enter " << size << " el" << endl;
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+        if(arr[i]%2 == 0)
+            sum += arr[i];
+        continue;
+    }
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
