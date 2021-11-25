a55b8cbea2f240e33c7783eded83e38061600d0a
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-25 22:13:16 +0200

    taskTwoDone

--- lab4/task2.cpp
+++ lab4/task2.cpp
@@ -3,25 +3,37 @@
 
 using namespace std;
 
+const int arrSize = 5;
+double arr[arrSize];
+int indexOfMax = 0;
+int indexOfMin = 0;
+
+int Max(){
+    for (int i = 1; i < arrSize; i++){
+        if (arr[i] > arr[indexOfMax])
+            indexOfMax = i;
+    }
+    return indexOfMax;
+}
+
+int Min(){
+    for (int i = 1; i < arrSize; i++){
+        if (arr[i] < arr[indexOfMin])
+            indexOfMin = i;
+    } 
+    return indexOfMax;
+}
+
 int main(){
-    const int arrSize = 5;
-    double arr[arrSize];
-    int indexOfMax = 0;
-    int indexOfMin = 0;
-    for(int i = 0; i < arrSize; i++){
+    for (int i = 0; i < arrSize; i++){
         cout << "Enter the " << i << " el" << endl;
         cin >> arr[i];
     }
-    for (int i = 1; i < arrSize; i++) 
-    {
-        if (arr[i] > arr[indexOfMax])
-            indexOfMax = i;
-        if (arr[i] < arr[indexOfMin])
-            indexOfMin = i;
-    }
+    Max();
+    Min();
     cout << "IndexOfMax = " << indexOfMax << " max = " << arr[indexOfMax] << endl;
     cout << "IndexOfMin = " << indexOfMin << " min = " << arr[indexOfMin] << endl;
-    cout << "max + min = " << arr[indexOfMax] + arr[indexOfMin]<< endl;
+    cout << "max + min = " << arr[indexOfMax] + arr[indexOfMin] << endl;
 
     return 0;
 }
