0996d323bbe5aecd6cf03a7bdcc678cfa8cb0ed2
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-26 14:56:41 +0200

    taskEightDone

--- lab4/task8.cpp
+++ lab4/task8.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int main(){
     int size = 5;
-    int arr[size];
+    double arr[size];
     int sum = 0;
     cout << "Enter " << size << " el" << endl;
     for (int i = 0; i < size; i++){
         cin >> arr[i];
-        if(i % 2 == 0)
+        if(i % 2 != 0)
             sum += arr[i];
-        continue;
     }
     cout << "Sum = " << sum << endl;
 }
