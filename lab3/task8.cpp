ba235a45dbb03e1ecf195d0e4489880f07f0f592
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-26 14:53:34 +0200

    taskEightDone

--- lab3/task8.cpp
+++ lab3/task8.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int taskEight(int n){
     int res = 1, i = 1;
-    while(i <= n){
+    while(i <= 2 * n){
         if (i % 2 != 0)
             res *= i;
         i++;
@@ -13,8 +13,8 @@ int taskEight(int n){
 }
 
 int main(){
+    cout << taskEight(2) << endl;
+    cout << taskEight(4) << endl;
     cout << taskEight(5) << endl;
-    cout << taskEight(7) << endl;
-    cout << taskEight(10) << endl;
     return 0;
 }
