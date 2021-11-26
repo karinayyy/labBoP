9ecc9f24db230fd7d55642000cb9cfa761527b99
karpenko-karina <karinakarpenko.k@gmail.com>
2021-11-26 14:53:54 +0200

    taskNineDone

--- lab3/task9.cpp
+++ lab3/task9.cpp
@@ -3,17 +3,19 @@
 
 using namespace std;
 
-double sum(int x, int n){
-    double y = 0;
-    for(int i = 0; i < n; i++){
-        y += ((x^n)/(n))+sum(x,n-1);
+double sum(double x){
+    double  y = 0;
+    int i =1;
+    for(double z = 1; z > 0.00001; i++){
+        y += z;
+        z *= x / i;
     }
-    return y + 1;
+    return y;
 }
 
 int main(){
-    cout << sum(1, 1) << endl;
-    cout << sum(2, 5) << endl;
-    cout << sum(5, 2) << endl;
+    cout << sum(1) << endl;
+    cout << sum(2) << endl;
+    cout << sum(5) << endl;
     return 0;
 }
