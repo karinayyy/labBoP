1b06fc76e4e7830de4ff644e6f2de9309e9a504f
karpenko-karina <karinakarpenko.k@gmail.com>
2021-12-22 00:31:50 +0200

    taskThreeDone

--- lab6/task3/dop1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "dopTasks.h"
-
-int main(){
-    signum(5) ; 
-    signum(0) ;
-    signum(-15);
-    return 0;
-}
--- lab6/task3/dopTasks.cpp
+++ lab6/task3/dopTasks.cpp
@@ -1,7 +1,108 @@
 #include <iostream>
+#include <cmath>
 #include "dopTasks.h"
 
 using namespace std;
+
+void minMax(int i){
+    static int min = i;
+    static int max = i;
+    if (i < min)
+        min = i;
+    if(i > max)
+        max = i;
+    cout << "min = " << min << endl << "max = " << max << endl;
+}
+
+double sum(int m, double x){
+    if(m < 1){
+        return 1;
+    } else{
+        return (x + m) * sum(m - 1, x); // n = 4 x = 5 (4+5)(3+5)(2+5)(1+5)=3024
+    }
+}
+
+double sum3(double x){
+    return x;
+}
+
+double sum3(double x, double y){
+    return x * y;
+}
+
+double sum3(){
+    return 1;
+}
+
+double sum31(double x, double y){
+    return x * y;
+}
+
+
+double taskTwo( double a4,
+                double b4,
+                double c4,
+                double& x14,
+                double& x24){
+    if(a4 == 0){
+        if(b4 == 0){
+            if(c4 == 0)
+                return -1;
+            else
+                return 0;
+        }else{
+            x14 = -c4/b4;
+            return 1;
+        }
+    }else{
+        double d = pow(b4, 2) - (4 * a4 * c4);
+        if(d < 0)
+            return 0;
+        if(d > 0){
+            d = sqrt(d);
+            x14 = (-b4 + d) / (2 * a4);
+            x24 = (-b4 - d) / (2 * a4);
+            return 2;
+        }else{
+            x14 = -b4 / (2 * a4);
+            return 1;
+        }
+    }
+}
+
+
+bool readData(double& a, double& b, double& h, double& n){
+    cout << "Введите входные данные (интервал a-b(a <= b), шаг h > 0, n > 1)" << endl;
+    cin >> a >> b >> h >> n;
+    if (a <= b && h > 0 && n > 1){
+        return true;
+    }
+    else{
+        cout << "Введите: (a <= b)&&(h > 0)&&(n > 1) " << endl;
+        return false;
+    }
+}
+
+double yFunc(double x, double n){
+    double y;
+    if (x < 0) {
+        y = 1;
+        for (int i = 1; i <= (n - 1); i++){
+            double s = 0;
+            for (int j = 1; j <= n; j++){
+                s += pow((x - pow(i, 2) + j), 2);
+            }
+            y *= s;
+        }
+    }
+    else if (x >= 0) {
+        y = 0;
+        for (int i = 0; i <= (n - 1); i++){
+            y += (x - 1) / (i + 1);
+        }
+    }
+    return y;
+}
 
 void signum(double x){
     if(x < 0)
@@ -67,4 +168,43 @@ void nod(int a, int b, int c){
         }
     }
     cout << a << endl;
+}
+
+
+double sumOfPowers(double q, int w){
+    double sum = 0;
+    double power = 1;
+    // Степені та суму можна розрахувати за один цикл:
+    for (int i = 0; i < w; i++){
+        power *= q;
+        sum += power;
+    }
+    return sum;
+}
+
+int add(int p){
+    static int sum = 0;
+    sum += p;
+    return sum;
+}
+
+
+int read(){
+    int g;
+    cout << "Input n:";
+    cin >> g;
+    return g;
+}
+
+int calc(int g){
+    int f = 0;
+    for (int i = 1; i <= g; i++)
+    {
+       f += i * i;
+    }
+    return f;
+}
+
+void write(int f){
+    cout << "y = " << f;
 }
