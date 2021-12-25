ab1fadcb8e324fa3f661744cf84eb38364b40603
karpenko-karina <karinakarpenko.k@gmail.com>
2021-12-25 20:49:34 +0200

    lab1

---
+++ lab1/task1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    double inch, mm;
+    cout << "Hello world!" << endl;
+    cout << "Введите значение в дюймах, чтобы получить миллиметры " << endl;
+    cin >> inch;
+    mm = inch * 25.4;
+    inch >= 0 ? cout << "Значение в миллиметрах = " << mm << " мм"<< endl : cout << "Введены неправильные данные" << endl;
+    return 0;
+}
