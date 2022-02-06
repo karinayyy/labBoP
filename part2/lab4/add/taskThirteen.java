cb3ea4845f2956d699539ae1e31b143c7b02667a
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 19:33:54 +0200

    taskThirteenDone

--- part2/lab4/add/taskThirteen.java
+++ part2/lab4/add/taskThirteen.java
@@ -3,17 +3,21 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskThirteen {
+    public static int mult(int n){
+        int res = 1;
+        for(int i = 1; i <= n; i++){
+            if(i % 2 != 0)
+                res *= i;
+        }
+        return res;
+    }
     public static void main(String[] args){
         System.out.println("Enter number");
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt(), res = 1;
+        int n = scan.nextInt();
         scan.close();
         if(n > 0){
-            for(int i = 1; i <= n; i++){
-                if(i % 2 != 0)
-                    res *= i;
-            }
-            System.out.println("res = " + res);
+            System.out.println("res = " + mult(n));
         }else   
             System.out.println("n <= 0 err");
     }
