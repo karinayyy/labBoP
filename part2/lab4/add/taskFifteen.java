7faaf4176a017229d4d7090307e3429b71aa82be
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 19:12:31 +0200

    taskFifteenDone

---
+++ part2/lab4/add/taskFifteen.java
@@ -0,0 +1,23 @@
+package part2.lab4.add;
+
+import java.util.Scanner;
+
+public class taskFifteen {
+    public static int sum(int m, int n){
+        int res = 0;
+        for(int i = n; i > m; i--)
+            res+=i;
+        return res;
+    }
+    public static void main(String[] args){
+        System.out.println("Enter number m < n");
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        scan.close();
+        if(m < n){
+            System.out.println("res " + sum(m, n));
+        }else
+            System.out.println("err m>=n");
+    }
+}
