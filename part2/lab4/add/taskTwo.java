3f48c8c31dc8fb3b2d653450e129c32071108408
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 20:41:37 +0200

    taskTwoDone

--- part2/lab4/add/taskTwo.java
+++ part2/lab4/add/taskTwo.java
@@ -3,17 +3,18 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskTwo {
+    public static double sqrt(int a, int b){
+        return Math.sqrt(a*b);
+    }
     public static void main(String[] args){
         int a, b;
-        double res;
         System.out.println("Enter a, b");
         Scanner scan = new Scanner(System.in);
         a = scan.nextInt();
         b = scan.nextInt();
         scan.close();
         if(a>=0 && b>=0){
-            res = Math.sqrt(a*b);
-            System.out.println("Result = " + res);
+            System.out.println("Result = " + sqrt(a, b));
         }else{
             System.out.println("Wrong data");
         }
