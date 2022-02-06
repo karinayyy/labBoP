04525984458fa06d1d20c8396347cfbd9f0d018e
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 20:50:51 +0200

    taskSixDone

--- part2/lab4/add/taskSix.java
+++ part2/lab4/add/taskSix.java
@@ -3,17 +3,21 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskSix {
+    public static int sum(int n){
+        int sum = 0;
+        for(int i = n; i != 0; i /= 10){ 
+            if(i % 2 != 0)
+                sum += (i % 10); 
+        }
+        return sum;
+    }
     public static void main(String[] args){
         System.out.println("Enter number");
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt(), sum = 0;
+        int n = scan.nextInt();
         scan.close();
         if(n > 0){
-            for(int i = n; i != 0; i /= 10){ 
-                if(i % 2 != 0)
-                    sum += (i % 10); 
-            } 
-            System.out.println("Sum = " + sum);
+            System.out.println("Sum = " + sum(n));
         }else
             System.out.println("err n <= 0");
     }
