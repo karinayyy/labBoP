e6de185a4cf45ded8298065d11f19aff00f10dee
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 19:35:24 +0200

    taskTwelveDone

--- part2/lab4/add/taskTwelve.java
+++ part2/lab4/add/taskTwelve.java
@@ -3,12 +3,15 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskTwelve {
-        public static void main(String[] args){
-            System.out.println("Enter num and pow");
-            Scanner scan = new Scanner(System.in);
-            double num = scan.nextDouble();
-            int pow = scan.nextInt();
-            scan.close();
-            System.out.println(num + "^" + pow + " = " + Math.pow(num, pow));
-        }
+    public static double power(double num, int pow){
+        return Math.pow(num, pow);
+    }
+    public static void main(String[] args){
+        System.out.println("Enter num and pow");
+        Scanner scan = new Scanner(System.in);
+        double num = scan.nextDouble();
+        int pow = scan.nextInt();
+        scan.close();
+        System.out.println(num + "^" + pow + " = " + power(num, pow));
+    }
 }
