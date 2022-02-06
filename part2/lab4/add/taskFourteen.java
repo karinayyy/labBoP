42c9d080aac6e1e6869597d7ce5b8fe4ce40d93b
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 19:32:44 +0200

    taskFourteenDone

--- part2/lab4/add/taskFourteen.java
+++ part2/lab4/add/taskFourteen.java
@@ -3,17 +3,20 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskFourteen {
+    public static int sum(int m, int n){
+        int res = 1;
+        for(int i = n; i > m; i--)
+            res*=i;
+        return res;
+    }
     public static void main(String[] args){
         System.out.println("Enter number m < n");
         Scanner scan = new Scanner(System.in);
         int m = scan.nextInt();
         int n = scan.nextInt();
-        int res = 1;
         scan.close();
         if(m < n){
-            for(int i = n; i > m; i--)
-                res*=i;
-            System.out.println("res " + res);
+            System.out.println("res " + sum(m, n));
         }else
             System.out.println("err m>=n");
     }
