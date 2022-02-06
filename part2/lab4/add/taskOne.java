b4b52f74aa927b6fa3566fbbb90faec37b1ede58
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 20:39:40 +0200

    taskOneDone

--- part2/lab4/add/taskOne.java
+++ part2/lab4/add/taskOne.java
@@ -3,16 +3,16 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskOne {
-
+    public static double mult(int a, int b){
+        return (Math.pow(a, 2) + Math.pow(b, 2))/2;
+    }
     public static void main(String[] args){
         int a, b;
-        double res;
         System.out.println("Enter a, b");
         Scanner scan = new Scanner(System.in);
         a = scan.nextInt();
         b = scan.nextInt();
         scan.close();
-        res = (Math.pow(a, 2) + Math.pow(b, 2))/2;
-        System.out.println("Result = " + res);
+        System.out.println("Result = " + mult(a, b));
     }    
 }
