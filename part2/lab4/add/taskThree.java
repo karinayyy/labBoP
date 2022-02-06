c2ccde409ff793d5470c310d3dfd4a3ef96c9956
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 20:42:29 +0200

    taskThreeDone

--- part2/lab4/add/taskThree.java
+++ part2/lab4/add/taskThree.java
@@ -3,17 +3,21 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskThree {
+    public static int mult(int n){
+        int mult = 1;
+        for(int i = n; i != 0; i /= 10){ 
+            if(i % 10 != 0)
+                mult *= (i % 10); 
+        }
+        return mult;
+    }
     public static void main(String[] args){
         System.out.println("Enter number");
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt(), mult = 1;
+        int n = scan.nextInt();
         scan.close();
         if(n > 0){
-            for(int i = n; i != 0; i /= 10){ 
-                if(i % 10 != 0)
-                    mult *= (i % 10); 
-            }
-        System.out.println("Mult = " + mult);
+        System.out.println("Mult = " + mult(n));
         }else
             System.out.println("err n <= 0");
     }
