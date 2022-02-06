f8eca0104d75f518439a9a888b0967e9544a3370
karpenko-karina <karinakarpenko.k@gmail.com>
2022-02-06 19:40:53 +0200

    taskTenDone

--- part2/lab4/add/taskTen.java
+++ part2/lab4/add/taskTen.java
@@ -3,15 +3,18 @@ package part2.lab4.add;
 import java.util.Scanner;
 
 public class taskTen {
+    public static int isSqrt(double num){
+        return (Math.sqrt(num) % 1 == 0) ? 1 : 0;
+    }
     public static void main(String[] args){
         System.out.println("Enter num");
         Scanner scan = new Scanner(System.in);
         double num = scan.nextInt();
         scan.close();
         if(num >= 0){
-            if(Math.sqrt(num) % 1 == 0)
+            if(isSqrt(num) == 1)
                 System.out.println("yes");
-            else
+            else    
                 System.out.println("no");
         }else
             System.out.println("err n<0");
